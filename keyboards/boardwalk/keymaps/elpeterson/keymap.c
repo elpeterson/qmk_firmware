ac8f8a59c0366fef71e748c2b34a04e3efb6831e
Eric Peterson <eric.peterson@motorolasolutions.com>
2019-04-08 10:40:07 -0400

    updated remaining layers

--- keyboards/boardwalk/keymaps/elpeterson/keymap.c
+++ keyboards/boardwalk/keymaps/elpeterson/keymap.c
@@ -9,7 +9,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     /* ALPHAS
      * .-----------------------------------------------------------------------------------------------------------------------------.
-     * | ESC    | 1      | 2      | 3      | 4      | 5      | =      | -      | 6      | 7      | 8      | 9      | 0      | BACKSP |
+     * | GESC   | 1      | 2      | 3      | 4      | 5      | =      | -      | 6      | 7      | 8      | 9      | 0      | BSPC   |
      * |--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+-----------------|
      * | TAB    | Q      | W      | E      | R      | T      | [      | ]      | Y      | U      | I      | O      | P      | '      |
      * |--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+-----------------+--------|
@@ -17,21 +17,21 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * |--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------------------------+--------|
      * | LCTRL  | Z      | X      | C      | V      | B      | BSLS   | UP     | N      | M      | ,      | .      | /      | RSHIFT |
      * |--------+--------+--------+--------+--------+-----------------+--------+--------+--------+-----------------+--------+--------|
-     * | LATL   | LGUI   | (NUM)  | (FN)   |      SPACE      | LEFT   | DOWN   | RIGHT  | WIN    | RCTL   | MUTE   | VOL-   | VOL+   |
+     * | LATL   | LGUI   | (NUM)  | (FN)   |       SPC       | LEFT   | DOWN   | RIGHT  | WIN    | RCTL   | MUTE   | VOL-   | VOL+   |
      * '-----------------------------------------------------------------------------------------------------------------------------'
      */
 
     [_ALPHAS] = LAYOUT_elpeterson(\
-        KC_ESC,  KC_1,    KC_2,     KC_3,    KC_4,    KC_5, KC_EQL,  KC_MINS, KC_6,     KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC, \
-        KC_TAB,  KC_Q,    KC_W,     KC_E,    KC_R,    KC_T, KC_LBRC, KC_RBRC, KC_Y,     KC_U,    KC_I,    KC_O,    KC_P,    KC_QUOT, \
-        KC_LSFT, KC_A,    KC_S,     KC_D,    KC_F,    KC_G, KC_HOME, KC_END,  KC_H,     KC_J,    KC_K,    KC_L,    KC_SCLN, KC_ENT, \
-        KC_LCTL, KC_Z,    KC_X,     KC_C,    KC_V,    KC_B, KC_BSLS, KC_UP,   KC_N,     KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, \
-        KC_LALT, KC_LGUI, TO(_NUM), OSL(2),  KC_SPC,        KC_LEFT, KC_DOWN, KC_RGHT,  KC_RGUI, KC_RCTL, KC_MUTE, KC_VOLD, KC_VOLU \
+        KC_GESC,  KC_1,    KC_2,     KC_3,      KC_4,    KC_5, KC_EQL,  KC_MINS, KC_6,     KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC, \
+        KC_TAB,   KC_Q,    KC_W,     KC_E,      KC_R,    KC_T, KC_LBRC, KC_RBRC, KC_Y,     KC_U,    KC_I,    KC_O,    KC_P,    KC_QUOT, \
+        KC_LSFT,  KC_A,    KC_S,     KC_D,      KC_F,    KC_G, KC_HOME, KC_END,  KC_H,     KC_J,    KC_K,    KC_L,    KC_SCLN, KC_ENT, \
+        KC_LCTL,  KC_Z,    KC_X,     KC_C,      KC_V,    KC_B, KC_BSLS, KC_UP,   KC_N,     KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, \
+        KC_LALT,  KC_LGUI, TO(_NUM), OSL(_FNC), KC_SPC,        KC_LEFT, KC_DOWN, KC_RGHT,  KC_RGUI, KC_RCTL, KC_MUTE, KC_VOLD, KC_VOLU \
     ),
 
     /* NUM
      * .-----------------------------------------------------------------------------------------------------------------------------.
-     * |        |        |        |        |        |        |        |        |        |        | BACKSP | /      | *      | -      |
+     * |        |        |        |        |        |        |        |        |        |        | BSPC   | /      | *      | -      |
      * |--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+-----------------|
      * |        |        |        |        |        |        |        |        |        |        | 7      | 8      | 9      | +      |
      * |--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+-----------------+--------|
@@ -39,21 +39,21 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * |--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------------------------+--------|
      * |        |        |        |        |        |        |        |        |        |        | 1      | 2      | 3      | =      |
      * |--------+--------+--------+--------+--------+-----------------+--------+--------+--------+-----------------+--------+--------|
-     * |        |        |        |        |        |                 |        |(ALPHAS)|        | 0      | 0      | .      | Enter  |
+     * |        |        |(ALPHAS)|        |        |                 |        |        |        | 0      | 0      | .      | Enter  |
      * '-----------------------------------------------------------------------------------------------------------------------------'
      */
 
     [_NUM] = LAYOUT_elpeterson(\
-        KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,       KC_NO, KC_BSPC, KC_PSLS, KC_PAST, KC_PMNS, \
-        KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,       KC_NO, KC_P7,   KC_P8,   KC_P9,   KC_PPLS, \
-        KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,       KC_NO, KC_P4,   KC_P5,   KC_P6,   KC_PCMM, \
-        KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,       KC_NO, KC_P1,   KC_P2,   KC_P3,   KC_PEQL, \
-        KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,        KC_NO, TO(_ALPHAS), KC_NO, KC_P0,   KC_P0,   KC_PDOT, KC_ENT \
+        KC_NO, KC_NO, KC_NO,       KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_BSPC, KC_PSLS, KC_PAST, KC_PMNS, \
+        KC_NO, KC_NO, KC_NO,       KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_P7,   KC_P8,   KC_P9,   KC_PPLS, \
+        KC_NO, KC_NO, KC_NO,       KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_P4,   KC_P5,   KC_P6,   KC_PCMM, \
+        KC_NO, KC_NO, KC_NO,       KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_P1,   KC_P2,   KC_P3,   KC_PEQL, \
+        KC_NO, KC_NO, TO(_ALPHAS), KC_NO, KC_NO, KC_NO,        KC_NO, KC_NO, KC_NO, KC_P0,   KC_P0,   KC_PDOT, KC_ENT \
     ),
 
     /* FNC
      * .-----------------------------------------------------------------------------------------------------------------------------.
-     * |        | F1     | F2     | F3     | F4     | F5     |        |        | F6     | F7     | F8     | F9     | F10    |        |
+     * |        | F1     | F2     | F3     | F4     | F5     |        |        | F6     | F7     | F8     | F9     | F10    | DEL    |
      * |--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+-----------------|
      * |        |        |        |        |        |        |        |        |        |        |        |        |        |        |
      * |--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+-----------------+--------|
@@ -61,16 +61,16 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * |--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------------------------+--------|
      * |        |        |        |        |        |        |        |        |        |        |        |        |        |        |
      * |--------+--------+--------+--------+--------+-----------------+--------+--------+--------+-----------------+--------+--------|
-     * |        |        |        |        |        |                 |        | (RGB)  |        |        |        |        |(ALPHAS)|
+     * |        |        |(ALPHAS)| (RGB)  |        |                 |        |        |        |        |        |        |        |
      * '-----------------------------------------------------------------------------------------------------------------------------'
      */
 
     [_FNC] = LAYOUT_elpeterson(\
-        KC_NO, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_NO, KC_NO, KC_F6,    KC_F7, KC_F8, KC_F9, KC_F10, KC_NO, \
-        KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,    KC_NO, KC_NO, KC_NO, KC_NO,  KC_NO, \
-        KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,    KC_NO, KC_NO, KC_NO, KC_NO,  KC_NO, \
-        KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,    KC_NO, KC_NO, KC_NO, KC_NO,  KC_NO, \
-        KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,        KC_NO, TO(_RGB), KC_NO, KC_NO, KC_NO, KC_NO,  TO(_ALPHAS) \
+        KC_NO, KC_F1, KC_F2,       KC_F3,    KC_F4, KC_F5, KC_NO, KC_NO, KC_F6,    KC_F7, KC_F8, KC_F9, KC_F10, KC_DEL, \
+        KC_NO, KC_NO, KC_NO,       KC_NO,    KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,    KC_NO, KC_NO, KC_NO, KC_NO,  KC_NO, \
+        KC_NO, KC_NO, KC_NO,       KC_NO,    KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,    KC_NO, KC_NO, KC_NO, KC_NO,  KC_NO, \
+        KC_NO, KC_NO, KC_NO,       KC_NO,    KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,    KC_NO, KC_NO, KC_NO, KC_NO,  KC_NO, \
+        KC_NO, KC_NO, TO(_ALPHAS), TO(_RGB), KC_NO, KC_NO,        KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,  KC_NO \
     ),
 
     /* RGB
@@ -83,15 +83,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * |--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------------------------+--------|
      * |        | BRGHT- | BRGHT+ |        |        |        |        |        |        |        |        |        |        |        |
      * |--------+--------+--------+--------+--------+-----------------+--------+--------+--------+-----------------+--------+--------|
-     * |        |        |        |        |        |                 |        |        |        |        |        |        |(ALPHAS)|
+     * |        |        |(ALPHAS)|        |        |                 |        |        |        |        |        |        |        |
      * '-----------------------------------------------------------------------------------------------------------------------------'
      */
 
     [_RGB] = LAYOUT_elpeterson(\
-        RGB_TOG, RGB_M_P, RGB_M_B, RGB_M_R, RGB_M_SW, RGB_M_SN, RGB_M_K, RGB_M_X, RGB_M_G, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, \
-        KC_NO,   RGB_HUD, RGB_HUI, KC_NO,   KC_NO,    KC_NO,    KC_NO,   KC_NO,   KC_NO,   KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, \
-        KC_NO,   RGB_SAD, RGB_SAI, KC_NO,   KC_NO,    KC_NO,    KC_NO,   KC_NO,   KC_NO,   KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, \
-        KC_NO,   RGB_VAD, RGB_VAI, KC_NO,   KC_NO,    KC_NO,    KC_NO,   KC_NO,   KC_NO,   KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, \
-        KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,    KC_NO,             KC_NO,   KC_NO,   KC_NO, KC_NO, KC_NO, KC_NO, TO(_ALPHAS) \
+        RGB_TOG, RGB_M_P, RGB_M_B,     RGB_M_R, RGB_M_SW, RGB_M_SN, RGB_M_K, RGB_M_X, RGB_M_G, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, \
+        KC_NO,   RGB_HUD, RGB_HUI,     KC_NO,   KC_NO,    KC_NO,    KC_NO,   KC_NO,   KC_NO,   KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, \
+        KC_NO,   RGB_SAD, RGB_SAI,     KC_NO,   KC_NO,    KC_NO,    KC_NO,   KC_NO,   KC_NO,   KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, \
+        KC_NO,   RGB_VAD, RGB_VAI,     KC_NO,   KC_NO,    KC_NO,    KC_NO,   KC_NO,   KC_NO,   KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, \
+        KC_NO,   KC_NO,   TO(_ALPHAS), KC_NO,   KC_NO,    KC_NO,             KC_NO,   KC_NO,   KC_NO, KC_NO, KC_NO, KC_NO, KC_NO \
     )
 };
