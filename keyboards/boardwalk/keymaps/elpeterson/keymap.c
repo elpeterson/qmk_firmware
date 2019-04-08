f2d3787e98deb6a439be1280535d8f529961152f
Eric Peterson <elpeterson16@gmail.com>
2019-04-07 21:03:19 -0400

    layout arrangement updates

--- keyboards/boardwalk/keymaps/elpeterson/keymap.c
+++ keyboards/boardwalk/keymaps/elpeterson/keymap.c
@@ -26,7 +26,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB,  KC_Q,    KC_W,     KC_E,    KC_R,    KC_T, KC_LBRC, KC_RBRC, KC_Y,     KC_U,    KC_I,    KC_O,    KC_P,    KC_QUOT, \
         KC_LSFT, KC_A,    KC_S,     KC_D,    KC_F,    KC_G, KC_HOME, KC_END,  KC_H,     KC_J,    KC_K,    KC_L,    KC_SCLN, KC_ENT, \
         KC_LCTL, KC_Z,    KC_X,     KC_C,    KC_V,    KC_B, KC_BSLS, KC_UP,   KC_N,     KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, \
-        KC_LALT, KC_LGUI, TO(_NUM), OSL(2),  KC_SPC,        KC_LEFT, KC_DOWN, KC_RGHT,  KC_RGUI, KC_CTRL, KC_MUTE, KC_VOLD, KC_VOLU \
+        KC_LALT, KC_LGUI, TO(_NUM), OSL(2),  KC_SPC,        KC_LEFT, KC_DOWN, KC_RGHT,  KC_RGUI, KC_RCTL, KC_MUTE, KC_VOLD, KC_VOLU \
     ),
 
     /* NUM
