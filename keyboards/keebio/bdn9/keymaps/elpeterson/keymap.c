66f14a32e1c523e820344cd1c24985c8846a4b56
Eric Peterson <elpeterson16@gmail.com>
2019-09-07 23:49:58 -0400

    updated whatever for thing

--- keyboards/keebio/bdn9/keymaps/elpeterson/keymap.c
+++ keyboards/keebio/bdn9/keymaps/elpeterson/keymap.c
@@ -1,46 +1,49 @@
-/* Copyright 2019 Danny Nguyen <danny@keeb.io>
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 2 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
 #include QMK_KEYBOARD_H
 
+enum layer {
+    MACRO,
+    ARROW,
+    GLOW,
+};
+
+#ifdef AUDIO_ENABLE
+  #define STARTUP_SONG SONG(AG_SWAP_SONG)
+  #define GOODBYE_SONG SONG(AG_NORM_SONG)
+#endif
+
+/* Toggle between layers */
+#define TOL0 TO(MACRO)
+#define TOL1 TO(ARROW)
+#define TOL2 TO(GLOW)
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-    [0] = LAYOUT(
-        KC_MUTE, KC_HOME,   KC_MPLY, \
-        MO(1),   KC_UP,   RGB_MOD, \
-        KC_LEFT, KC_DOWN, KC_RGHT \
+    [MACRO] = LAYOUT(
+        KC_MUTE, TOL1,   KC_MUTE,
+        KC_F7,   KC_F8,  KC_F9,
+        KC_F10,  KC_F11, KC_F12
     ),
-    [1] = LAYOUT(
-        RESET,   BL_STEP, KC_STOP, \
-        _______, KC_HOME, RGB_MOD, \
-        KC_MPRV, KC_END,  KC_MNXT \
+    [ARROW] = LAYOUT(
+        RESET,   TOL2,    RESET,
+        KC_HOME, KC_UP,   KC_END,
+        KC_LEFT, KC_DOWN, KC_RGHT
+    ),
+    [GLOW] = LAYOUT(
+        RGB_TOG, TOL0,    RGB_MOD,
+        RGB_HUI, RGB_SAI, RGB_VAI,
+        RGB_HUD, RGB_SAD, RGB_VAD
     ),
 };
 
 void encoder_update_user(uint8_t index, bool clockwise) {
-    if (index == 0) {
-        if (clockwise) {
-            tap_code(KC_VOLU);
-        } else {
-            tap_code(KC_VOLD);
-        }
-    }
-    else if (index == 1) {
-        if (clockwise) {
-            tap_code(KC_PGDN);
-        } else {
-            tap_code(KC_PGUP);
-        }
+    switch (index) {
+        /* Top-left encoder (volume): */
+        case 0:
+            tap_code(clockwise ? KC_VOLU : KC_VOLD);
+            break;
+    
+        /* Top-right encoder (page up and down): */
+        case 1:
+            tap_code(clockwise ? KC_PGUP: KC_PGDN);
+            break;
     }
 }
