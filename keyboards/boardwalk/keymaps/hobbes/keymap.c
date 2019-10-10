98a1399d3e4fbbd5567bbb37fc83e1f824ff32d2
Eric Peterson <elpeterson16@gmail.com>
2019-10-10 00:16:43 -0400

    more revisions to BDN9 and boardwalk

--- keyboards/boardwalk/keymaps/hobbes/keymap.c
+++ keyboards/boardwalk/keymaps/hobbes/keymap.c
@@ -26,10 +26,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      */
 
     [_QW] = LAYOUT_hobbes(\
-        KC_GESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_EQL,  KC_MINS, KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC, \
-        KC_TAB,   KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_LBRC, KC_RBRC, KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_QUOT, \
-        KC_LSFT,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_NO,   KC_NO,   KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_ENT, \
-        KC_LCTL,  KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_HOME, KC_END,  KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, \
+        KC_GESC,  KC_1,    KC_2,      KC_3,    KC_4,    KC_5,    KC_EQL,  KC_MINS, KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC, \
+        KC_TAB,   KC_Q,    KC_W,      KC_E,    KC_R,    KC_T,    KC_LBRC, KC_RBRC, KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_QUOT, \
+        KC_LSFT,  KC_A,    KC_S,      KC_D,    KC_F,    KC_G,    KC_NO,   KC_NO,   KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_ENT, \
+        KC_LCTL,  KC_Z,    KC_X,      KC_C,    KC_V,    KC_B,    KC_HOME, KC_END,  KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, \
         KC_LALT,  KC_LGUI, MEH(KC_Z), KC_SLCK, KC_SPC,  KC_SPC,  TG(1),   TG(2),   KC_DEL,  KC_MPLY, KC_MUTE, COG,     KC_RGUI, KC_RCTL \
     ),
 
@@ -48,33 +48,33 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      */
 
     [_RAISE] = LAYOUT_hobbes(\
-        KC_GRV,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_NO,   KC_NO,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_NO, \
-        KC_NO,    KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO, \
-        KC_NO,    KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO, \
-        KC_NO,    KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO, \
-        KC_NO,    KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_TRNS, KC_TRNS, KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO \
+        KC_GRV, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_NO,   KC_NO,   KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_NO, \
+        KC_NO,  KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,   KC_NO,   KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,  KC_NO, \
+        KC_NO,  KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,   KC_NO,   KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,  KC_NO, \
+        KC_NO,  KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,   KC_NO,   KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,  KC_NO, \
+        KC_NO,  KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_TRNS, KC_TRNS, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,  KC_NO \
     ),
 
     /* RGB
      * .-----------------------------------------------------------------------------------------------------------------------------.
-     * |        |        |        |        |        |        |        |        |        |        |        |        |        |        |
+     * | TOG    | Plain  | Breathe| Rainbow| Swirl  | Snake  | Knight | XMas   |Gradient| RGBTest|        |        |        |        |
      * |--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+-----------------|
-     * |        |        |        |        |        |        |        |        |        |        |        |        |        |        |
+     * |        | HUE-   | HUE+   |        |        |        |        |        |        |        |        |        |        |        |
      * |--------+--------+--------+--------+--------+--------|        |        |--------+--------+--------+-----------------+--------|
-     * |        |        |        |        |        |        |--------+--------|        |        |        |        |        |        |
+     * |        | SAT-   | SAT+   |        |        |        |--------+--------|        |        |        |        |        |        |
      * |--------+--------+--------+--------+--------+--------|        |        |--------+--------+--------------------------+--------|
-     * |        |        |        |        |        |        |        |        |        |        |        |        |        |        |
+     * |        | BRGHT- | BRGHT+ |        |        |        |        |        |        |        |        |        |        |        |
      * |--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+-----------------+--------+--------|
      * |        |        |        |        |                 | TRNS   | TRNS   |        |        |        |        |        |        |
      * '-----------------------------------------------------------------------------------------------------------------------------'
      */
 
     [_LOWER] = LAYOUT_hobbes(\
-        RGB_TOG, RGB_M_P, RGB_M_B, RGB_M_R, RGB_M_SW,RGB_M_SN,RGB_M_K, RGB_M_X, RGB_M_G, KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO, \
-        KC_NO,   RGB_HUD, RGB_HUI, KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO, \
-        KC_NO,   RGB_SAD, RGB_SAI, KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO, \
-        KC_NO,   RGB_VAD, RGB_VAI, KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO, \
-        KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_TRNS, KC_TRNS, KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO \
+        RGB_TOG, RGB_M_P, RGB_M_B, RGB_M_R, RGB_M_SW, RGB_M_SN, RGB_M_K, RGB_M_X, RGB_M_G, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, \
+        KC_NO,   RGB_HUD, RGB_HUI, KC_NO,   KC_NO,    KC_NO,    KC_NO,   KC_NO,   KC_NO,   KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, \
+        KC_NO,   RGB_SAD, RGB_SAI, KC_NO,   KC_NO,    KC_NO,    KC_NO,   KC_NO,   KC_NO,   KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, \
+        KC_NO,   RGB_VAD, RGB_VAI, KC_NO,   KC_NO,    KC_NO,    KC_NO,   KC_NO,   KC_NO,   KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, \
+        KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,    KC_NO,    KC_TRNS, KC_TRNS, KC_NO,   KC_NO, KC_NO, KC_NO, KC_NO, KC_NO \
     ),
 };
 
--- keyboards/keebio/bdn9/keymaps/elpeterson/keymap.c
+++ keyboards/keebio/bdn9/keymaps/elpeterson/keymap.c
@@ -12,38 +12,33 @@ enum layer {
 #endif
 
 /* Toggle between layers */
-#define TOL0 TO(MACRO)
-#define TOL1 TO(ARROW)
-#define TOL2 TO(GLOW)
+#define LYRTOG TG(MACRO)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [MACRO] = LAYOUT(
-        KC_MUTE, TOL1,   KC_MUTE,
-        KC_F7,   KC_F8,  KC_F9,
-        KC_F10,  KC_F11, KC_F12
-    ),
-    [ARROW] = LAYOUT(
-        RESET,   TOL2,    RESET,
+        KC_NO,   LYRTOG,  KC_NO,
         KC_HOME, KC_UP,   KC_END,
         KC_LEFT, KC_DOWN, KC_RGHT
     ),
-    [GLOW] = LAYOUT(
-        RGB_TOG, TOL0,    RGB_MOD,
-        RGB_HUI, RGB_SAI, RGB_VAI,
-        RGB_HUD, RGB_SAD, RGB_VAD
+    [ARROW] = LAYOUT(
+        RESET,   KC_TRNS, RESET,
+        KC_HOME, KC_UP,   KC_END,
+        KC_LEFT, KC_DOWN, KC_RGHT
     ),
 };
 
 void encoder_update_user(uint8_t index, bool clockwise) {
-    switch (index) {
-        /* Top-left encoder (volume): */
-        case 0:
-            tap_code(clockwise ? KC_VOLU : KC_VOLD);
-            break;
-    
-        /* Top-right encoder (page up and down): */
-        case 1:
-            tap_code(clockwise ? KC_PGUP: KC_PGDN);
-            break;
+  if (index == 0) { /* First encoder */
+    if (clockwise) {
+      tap_code(KC_VOLU);
+    } else {
+      tap_code(KC_VOLD);
     }
+  } else if (index == 1) { /* Second encoder */
+    if (clockwise) {
+      tap_code(KC_UP);
+    } else {
+      tap_code(KC_DOWN);
+    }
+  }
 }
