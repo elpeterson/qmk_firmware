a9eb6387588c3899f8ec892e79ac914fff5a25f9
Eric Peterson <elpeterson16@gmail.com>
2020-07-29 13:54:30 -0400

    Updated boardawlk with minor layout changes

--- keyboards/boardwalk/keymaps/hobbes/keymap.c
+++ keyboards/boardwalk/keymaps/hobbes/keymap.c
@@ -1,99 +1,97 @@
 #include QMK_KEYBOARD_H
 
-#define _QW 0
-#define _RAISE 1
-#define _LOWER 2
-
-enum custom_keycodes {
-  COG,
-  MIC,
-};
+#define _ALPHAS 0
+#define _NUM 1
+#define _FNC 2
+#define _RGB 3
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
-    /* QWERTY
+    /* ALPHAS
      * .-----------------------------------------------------------------------------------------------------------------------------.
      * | ESC    | 1      | 2      | 3      | 4      | 5      | =      | -      | 6      | 7      | 8      | 9      | 0      | BSPC   |
      * |--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+-----------------|
-     * | TAB    | Q      | W      | E      | R      | T      | {      |      } | Y      | U      | I      | O      | P      | '      |
-     * |--------+--------+--------+--------+--------+--------| [      |      ] |--------+--------+--------+-----------------+--------|
-     * | LSHFT  | A      | S      | D      | F      | G      |--------+--------| H      | J      | K      | L      | ;      | ENTER  |
-     * |--------+--------+--------+--------+--------+--------| HOME   | END    |--------+--------+--------------------------+--------|
-     * | LCTRL  | Z      | X      | C      | V      | B      |        |        | N      | M      | ,      | .      | /      | RSHIFT |
-     * |--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+-----------------+--------+--------|
-     * | LATL   | WIN    | REC    | MIC    |      SPACE      | RAISE  | LOWER  | DEL    | PAUSE  | MUTEVOL| COG    | WIN    | RCTRL  |
+     * | TAB    | Q      | W      | E      | R      | T      | [      | ]      | Y      | U      | I      | O      | P      | '      |
+     * |--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+-----------------+--------|
+     * | LSHFT  | A      | S      | D      | F      | G      | HOME   | END    | H      | J      | K      | L      | ;      | ENTER  |
+     * |--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------------------------+--------|
+     * | LCTRL  | Z      | X      | C      | V      | B      | BSLS   | UP     | N      | M      | ,      | .      | /      | RSHIFT |
+     * |--------+--------+--------+--------+--------+-----------------+--------+--------+--------+-----------------+--------+--------|
+     * | LATL   | LGUI   | (NUM)  | (FN)   |       SPC       | LEFT   | DOWN   | RIGHT  | CAP    | MUTE   | MIC    | WIN    | RCTL   |
      * '-----------------------------------------------------------------------------------------------------------------------------'
      */
 
-    [_QW] = LAYOUT_hobbes(\
-        KC_GESC,  KC_1,    KC_2,      KC_3,    KC_4,    KC_5,    KC_EQL,  KC_MINS, KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC, \
-        KC_TAB,   KC_Q,    KC_W,      KC_E,    KC_R,    KC_T,    KC_LBRC, KC_RBRC, KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_QUOT, \
-        KC_LSFT,  KC_A,    KC_S,      KC_D,    KC_F,    KC_G,    KC_NO,   KC_NO,   KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_ENT, \
-        KC_LCTL,  KC_Z,    KC_X,      KC_C,    KC_V,    KC_B,    KC_HOME, KC_END,  KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, \
-        KC_LALT,  KC_LGUI, MEH(KC_Z), KC_SLCK, KC_SPC,  KC_SPC,  TG(1),   TG(2),   KC_DEL,  KC_MPLY, KC_MUTE, COG,     KC_RGUI, KC_RCTL \
+    [_ALPHAS] = LAYOUT_hobbes(\
+        KC_ESC,   KC_1,    KC_2,     KC_3,      KC_4,    KC_5, KC_EQL,  KC_MINS, KC_6,     KC_7,       KC_8,    KC_9,   KC_0,    KC_BSPC, \
+        KC_TAB,   KC_Q,    KC_W,     KC_E,      KC_R,    KC_T, KC_LBRC, KC_RBRC, KC_Y,     KC_U,       KC_I,    KC_O,   KC_P,    KC_QUOT, \
+        KC_LSFT,  KC_A,    KC_S,     KC_D,      KC_F,    KC_G, KC_HOME, KC_END,  KC_H,     KC_J,       KC_K,    KC_L,   KC_SCLN, KC_ENT, \
+        KC_LCTL,  KC_Z,    KC_X,     KC_C,      KC_V,    KC_B, KC_BSLS, KC_UP,   KC_N,     KC_M,       KC_COMM, KC_DOT, KC_SLSH, KC_RSFT, \
+        KC_LALT,  KC_LGUI, TO(_NUM), OSL(_FNC), KC_SPC,        KC_LEFT, KC_DOWN, KC_RGHT,  LALT(KC_Z), KC_MUTE, KC_M,   KC_RGUI, KC_RCTL \
+    ),
+
+    /* NUM
+     * .-----------------------------------------------------------------------------------------------------------------------------.
+     * |        |        |        |        |        |        |        |        |        |        | BSPC   | /      | *      | -      |
+     * |--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+-----------------|
+     * |        |        |        |        |        |        |        |        |        |        | 7      | 8      | 9      | +      |
+     * |--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+-----------------+--------|
+     * |        |        |        |        |        |        |        |        |        |        | 4      | 5      | 6      | ,      |
+     * |--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------------------------+--------|
+     * |        |        |        |        |        |        |        |        |        |        | 1      | 2      | 3      | =      |
+     * |--------+--------+--------+--------+--------+-----------------+--------+--------+--------+-----------------+--------+--------|
+     * |        |        |(ALPHAS)|        |        |                 |        |        |        | 0      | 0      | .      | Enter  |
+     * '-----------------------------------------------------------------------------------------------------------------------------'
+     */
+
+    [_NUM] = LAYOUT_hobbes(\
+        KC_NO, KC_NO, KC_NO,       KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_BSPC, KC_PSLS, KC_PAST, KC_PMNS, \
+        KC_NO, KC_NO, KC_NO,       KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_P7,   KC_P8,   KC_P9,   KC_PPLS, \
+        KC_NO, KC_NO, KC_NO,       KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_P4,   KC_P5,   KC_P6,   KC_PCMM, \
+        KC_NO, KC_NO, KC_NO,       KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_P1,   KC_P2,   KC_P3,   KC_PEQL, \
+        KC_NO, KC_NO, TO(_ALPHAS), KC_NO, KC_NO, KC_NO,        KC_NO, KC_NO, KC_NO, KC_P0,   KC_P0,   KC_PDOT, KC_ENT \
     ),
 
     /* FNC
      * .-----------------------------------------------------------------------------------------------------------------------------.
-     * | GRV    | F1     | F2     | F3     | F4     | F5     | F11    | F12    | F6     | F7     | F8     | F9     | F10    |        |
+     * | GRV    | F1     | F2     | F3     | F4     | F5     |        |        | F6     | F7     | F8     | F9     | F10    | DEL    |
      * |--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+-----------------|
      * |        |        |        |        |        |        |        |        |        |        |        |        |        |        |
-     * |--------+--------+--------+--------+--------+--------|        |        |--------+--------+--------+-----------------+--------|
-     * |        |        |        |        |        |        |--------+--------|        |        |        |        |        |        |
-     * |--------+--------+--------+--------+--------+--------|        |        |--------+--------+--------------------------+--------|
+     * |--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+-----------------+--------|
      * |        |        |        |        |        |        |        |        |        |        |        |        |        |        |
-     * |--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+-----------------+--------+--------|
-     * |        |        |        |        |                 | TRNS   | TRNS   |        |        |        |        |        |        |
+     * |--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------------------------+--------|
+     * |        |        |        |        |        |        |        |        |        |        |        |        |        |        |
+     * |--------+--------+--------+--------+--------+-----------------+--------+--------+--------+-----------------+--------+--------|
+     * |        |        |(ALPHAS)| (RGB)  |        |                 |        |        |        |        |        |        |        |
      * '-----------------------------------------------------------------------------------------------------------------------------'
      */
 
-    [_RAISE] = LAYOUT_hobbes(\
-        KC_GRV, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_NO,   KC_NO,   KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_NO, \
-        KC_NO,  KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,   KC_NO,   KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,  KC_NO, \
-        KC_NO,  KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,   KC_NO,   KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,  KC_NO, \
-        KC_NO,  KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,   KC_NO,   KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,  KC_NO, \
-        KC_NO,  KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_TRNS, KC_TRNS, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,  KC_NO \
+    [_FNC] = LAYOUT_hobbes(\
+        KC_GRV, KC_F1, KC_F2,       KC_F3,    KC_F4, KC_F5, KC_NO, KC_NO, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_DEL, \
+        KC_NO, KC_NO,  KC_NO,       KC_NO,    KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,  KC_NO, \
+        KC_NO, KC_NO,  KC_NO,       KC_NO,    KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,  KC_NO, \
+        KC_NO, KC_NO,  KC_NO,       KC_NO,    KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,  KC_NO, \
+        KC_NO, KC_NO,  TO(_ALPHAS), TO(_RGB), KC_NO, KC_NO,        KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,  KC_NO \
     ),
 
     /* RGB
      * .-----------------------------------------------------------------------------------------------------------------------------.
      * | TOG    | Plain  | Breathe| Rainbow| Swirl  | Snake  | Knight | XMas   |Gradient| RGBTest|        |        |        |        |
      * |--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+-----------------|
-     * |        | HUE-   | HUE+   |        |        |        |        |        |        |        |        |        |        |        |
-     * |--------+--------+--------+--------+--------+--------|        |        |--------+--------+--------+-----------------+--------|
-     * |        | SAT-   | SAT+   |        |        |        |--------+--------|        |        |        |        |        |        |
-     * |--------+--------+--------+--------+--------+--------|        |        |--------+--------+--------------------------+--------|
+     * |        | HUE-   | HUE+   |        |        |        |        |        |        |        |        |        |        | '      |
+     * |--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+-----------------+--------|
+     * |        | SAT-   | SAT+   |        |        |        |        |        |        |        |        |        |        |        |
+     * |--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------------------------+--------|
      * |        | BRGHT- | BRGHT+ |        |        |        |        |        |        |        |        |        |        |        |
-     * |--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+-----------------+--------+--------|
-     * |        |        |        |        |                 | TRNS   | TRNS   |        |        |        |        |        |        |
+     * |--------+--------+--------+--------+--------+-----------------+--------+--------+--------+-----------------+--------+--------|
+     * |        |        |(ALPHAS)|        |        |                 |        |        |        |        |        |        |        |
      * '-----------------------------------------------------------------------------------------------------------------------------'
      */
 
-    [_LOWER] = LAYOUT_hobbes(\
-        RGB_TOG, RGB_M_P, RGB_M_B, RGB_M_R, RGB_M_SW, RGB_M_SN, RGB_M_K, RGB_M_X, RGB_M_G, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, \
-        KC_NO,   RGB_HUD, RGB_HUI, KC_NO,   KC_NO,    KC_NO,    KC_NO,   KC_NO,   KC_NO,   KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, \
-        KC_NO,   RGB_SAD, RGB_SAI, KC_NO,   KC_NO,    KC_NO,    KC_NO,   KC_NO,   KC_NO,   KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, \
-        KC_NO,   RGB_VAD, RGB_VAI, KC_NO,   KC_NO,    KC_NO,    KC_NO,   KC_NO,   KC_NO,   KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, \
-        KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,    KC_NO,    KC_TRNS, KC_TRNS, KC_NO,   KC_NO, KC_NO, KC_NO, KC_NO, KC_NO \
-    ),
-};
-
-bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-  switch (keycode) {
-    case COG:
-      if (record->event.pressed) {
-        // when keycode QMKBEST is pressed
-        SEND_STRING("THIS IS THE COG KEY");
-      }
-      return false;
-      break;
-    case MIC:
-      if (record->event.pressed) {
-        // when keycode QMKBEST is pressed
-        SEND_STRING("THIS IS THE MIC KEY");
-      }
-      return false;
-      break;
-  }
-  return true;
+    [_RGB] = LAYOUT_hobbes(\
+        RGB_TOG, RGB_M_P, RGB_M_B,     RGB_M_R, RGB_M_SW, RGB_M_SN, RGB_M_K, RGB_M_X, RGB_M_G, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, \
+        KC_NO,   RGB_HUD, RGB_HUI,     KC_NO,   KC_NO,    KC_NO,    KC_NO,   KC_NO,   KC_NO,   KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, \
+        KC_NO,   RGB_SAD, RGB_SAI,     KC_NO,   KC_NO,    KC_NO,    KC_NO,   KC_NO,   KC_NO,   KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, \
+        KC_NO,   RGB_VAD, RGB_VAI,     KC_NO,   KC_NO,    KC_NO,    KC_NO,   KC_NO,   KC_NO,   KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, \
+        KC_NO,   KC_NO,   TO(_ALPHAS), KC_NO,   KC_NO,    KC_NO,             KC_NO,   KC_NO,   KC_NO, KC_NO, KC_NO, KC_NO, KC_NO \
+    )
 };
